5d77cbba321bcfb603fa7d8ed0a0974d739e4049
Samuel Veras <samuel.bernardo.veras@gmail.com>
2023-11-28 15:53:12 -0300

    Página

---
+++ script.js
@@ -0,0 +1,45 @@
+function inverterNumero(num) {
+    let inverso = 0;
+    while (num > 0) {
+        inverso = (inverso * 10) + (num % 10);
+        num = Math.floor(num / 10);
+    }
+    return inverso;
+}
+
+function decimalParaBase(num, base) {
+    const letras = '0123456789ABCDEF'; // Dígitos até a base 16 (hexadecimal)
+    let resultado = [];
+    while (num > 0) {
+        resultado.unshift(letras[num % base]);
+        num = Math.floor(num / base);
+    }
+    return resultado;
+}
+
+function baseParaDecimal(num, base) {
+    const letras = '0123456789ABCDEF'; // Dígitos até a base 16 (hexadecimal)
+    let decimal = 0;
+    for (let i = 0; i < num.length; i++) {
+        decimal += letras.indexOf(num[i]) * Math.pow(base, num.length - 1 - i);
+    }
+    return decimal;
+}
+
+function baseArbitraria(num, base, novaBase) {
+    const decimal = baseParaDecimal(num, base);
+    return decimalParaBase(decimal, novaBase);
+}
+
+function converter() {
+    const numeroInput = document.getElementById('numeroInput').value.toUpperCase(); // Garantir que as letras sejam maiúsculas
+    const baseInput = parseInt(document.getElementById('baseInput').value);
+    const novaBaseInput = parseInt(document.getElementById('novaBaseInput').value);
+    
+    const numDecimal = parseInt(numeroInput, baseInput);
+    const numBaseConvertido = baseArbitraria(decimalParaBase(numDecimal, 10), 10, novaBaseInput);
+
+    // Exibe o resultado na página
+    const resultadoDiv = document.getElementById('resultado');
+    resultadoDiv.innerHTML = `O número ${numeroInput} de base ${baseInput} na base ${novaBaseInput} é ${numBaseConvertido.join('')}.`;
+}
