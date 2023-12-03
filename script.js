e80c2a7da95d7349cab60bd059d04d451c154834
goerll <estevaogoerlln@gmail.com>
2023-12-03 01:22:53 -0300

    Update Co-authored-by: SamVeras <SamVeras@users.noreply.github.com>

--- script.js
+++ script.js
@@ -43,3 +43,80 @@ function converter() {
     const resultadoDiv = document.getElementById('resultado');
     resultadoDiv.innerHTML = `${numeroInput}<sub>${baseInput}</sub> = ${numBaseConvertido.join('')}<sub>${novaBaseInput}</sub>`;
 }
+
+
+function operacaoAritmetica(num, num2, operacao) {
+    switch (operacao) {
+        case "SOMA":
+            return num + num2;
+        case "SUBT":
+            return num - num2;
+        case "MULT":
+            return num * num2;
+        case "DIVS":
+            return num / num2;
+        default:
+            return "Operação inválida";
+    }
+}
+
+function realizarOperacao() {
+  const numeroInput1 = document
+    .getElementById("numeroInput1")
+    .value.toUpperCase();
+  const baseInput1 = parseInt(document.getElementById("baseInput1").value);
+  const numeroInput2 = document
+    .getElementById("numeroInput2")
+    .value.toUpperCase();
+  const baseInput2 = parseInt(document.getElementById("baseInput2").value);
+  const operacao = document.getElementById("operacao").value;
+  const baseOutput = parseInt(document.getElementById("baseOutput").value);
+
+  const numDecimal1 = parseInt(numeroInput1, baseInput1);
+  const numDecimal2 = parseInt(numeroInput2, baseInput2);
+
+  let resultado;
+  let operacao_simbolo;
+  switch (operacao) {
+    case "SOMA":
+      resultado = numDecimal1 + numDecimal2;
+      operacao_simbolo = "+";
+      break;
+    case "SUBT":
+      resultado = numDecimal1 - numDecimal2;
+      operacao_simbolo = "-";
+      break;
+    case "MULT":
+      resultado = numDecimal1 * numDecimal2;
+      operacao_simbolo = "*";
+      break;
+    case "DIVS":
+      resultado = numDecimal1 / numDecimal2;
+      operacao_simbolo = "/";
+      break;
+    default:
+      resultado = "Operação inválida";
+  }
+
+  const resultadoBaseOutput = decimalParaBase(resultado, baseOutput);
+
+  document.getElementById("resultado").innerHTML = 
+  `${numeroInput1}<sub>${baseInput1}</sub> ${operacao_simbolo} ${numeroInput2}<sub>${baseInput2}</sub> = ${resultadoBaseOutput.join("")}<sub>${baseOutput}</sub>`;
+}
+
+function mudarBackground() {
+    const url = 'https://bing.biturl.top/?resolution=1920&format=json&index=random&mkt=zh-CN';
+  
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const imageUrl = data.url;
+        document.body.style.backgroundImage = `url(${imageUrl})`;
+      })
+      .catch(error => {
+        console.error('Error fetching background image:', error);
+      });
+  }
+  
+  // Call the function to change the background image
+  mudarBackground();
